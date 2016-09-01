e1ac9860031c395e92f7f8f96642191eb4de5941
Linus Walleij <linus.walleij@linaro.org>
2016-09-01 10:27:17 +0200

    UPSTREAM: iio: trigger: helpers to determine own trigger
    This adds a helper function to the IIO trigger framework:
    
    iio_trigger_using_own(): for an IIO device, this tells
      whether the device is using itself as a trigger.
      This is true if the indio device:
      (A) supplies a trigger and
      (B) has assigned its own buffer poll function to use this
          trigger.
    
    This helper function is good when constructing triggered,
    buffered drivers that can either use its own hardware *OR*
    an external trigger such as a HRTimer or even the trigger from
    a totally different sensor.
    
    Under such circumstances it is important to know for example
    if the timestamp from the same trigger hardware should be used
    when populating the buffer: if iio_trigger_using_own() is true,
    we can use this timestamp, else we need to pick a unique
    timestamp directly in the trigger handler.
    
    For this to work of course IIO devices registering hardware
    triggers must follow the convention to set the parent device
    properly, as as well as setting the parent of the IIO device
    itself.
    
    When a new poll function is attached, we check if the parent
    device of the IIO of the poll function is the same as the
    parent device of the trigger and in that case we conclude that
    the hardware is using itself as trigger.
    
    Cc: Giuseppe Barba <giuseppe.barba@st.com>
    Cc: Denis Ciocca <denis.ciocca@st.com>
    Cc: Crestez Dan Leonard <leonard.crestez@intel.com>
    Cc: Gregor Boirie <gregor.boirie@parrot.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    
    BUG=none
    TEST=cmpile, loaded on eve-arc-r
    
    (cherry picked from commit 702a7b8e064a93df0b63e9d718b666a9851088fc)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: I77118ba4071f5ed7a594d15566264c4e32ab2ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2645411
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Cheng-Hao Yang <chenghaoyang@chromium.org>

--- drivers/iio/industrialio-trigger.c
+++ drivers/iio/industrialio-trigger.c
@@ -230,6 +230,14 @@ static int iio_trigger_attach_poll_func(struct iio_trigger *trig,
 			goto out_free_irq;
 	}
 
+	/*
+	 * Check if we just registered to our own trigger: we determine that
+	 * this is the case if the IIO device and the trigger device share the
+	 * same parent device.
+	 */
+	if (pf->indio_dev->dev.parent == trig->dev.parent)
+		trig->attached_own_device = true;
+
 	return ret;
 
 out_free_irq:
@@ -254,6 +262,8 @@ static int iio_trigger_detach_poll_func(struct iio_trigger *trig,
 		if (ret)
 			return ret;
 	}
+	if (pf->indio_dev->dev.parent == trig->dev.parent)
+		trig->attached_own_device = false;
 	iio_trigger_put_irq(trig, pf->irq);
 	free_irq(pf->irq, pf);
 	module_put(pf->indio_dev->info->driver_module);
@@ -655,6 +665,12 @@ void devm_iio_trigger_unregister(struct device *dev,
 	WARN_ON(rc);
 }
 EXPORT_SYMBOL_GPL(devm_iio_trigger_unregister);
+
+bool iio_trigger_using_own(struct iio_dev *indio_dev)
+{
+	return indio_dev->trig->attached_own_device;
+}
+EXPORT_SYMBOL(iio_trigger_using_own);
 
 void iio_device_register_trigger_consumer(struct iio_dev *indio_dev)
 {
