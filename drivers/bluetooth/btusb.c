9f04354617a1676a99df8a3714c49aa2778fafd3
Hans de Goede <hdegoede@redhat.com>
2017-11-13 14:44:16 +0100

    UPSTREAM: Bluetooth: btusb: Add a Kconfig option to enable USB autosuspend by default
    On many laptops the btusb device is the only USB device not having USB
    autosuspend enabled, this causes not only the HCI but also the USB
    controller to stay awake, together using aprox. 0.4W of power.
    
    Modern ultrabooks idle around 6W (at 50% screen brightness), 3.5W for
    Apollo Lake devices. 0.4W is a significant chunk of this (7 / 11%).
    
    The btusb driver already contains code to allow enabling USB autosuspend,
    but currently leaves it up to the user / userspace to enable it. This
    means that for most people it will not be enabled, leading to an
    unnecessarily high power consumption.
    
    Since enabling it is not entirely without risk of regressions, this
    commit adds a Kconfig option so that Linux distributions can choose to
    enable it by default. This commit also adds a module option so that when
    distros receive bugs they can easily ask the user to disable it again
    for easy debugging.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit eff2d68ca7388ee1c08811c6bbf4d8587cba01da)
    Signed-off-by: Alex Lu <alex_lu@realsil.com.cn>
    
    BUG=b:139382142
    TEST=make menuconfig
    
    Change-Id: I0c74d04dcf4a5a92a63301db00248f0d6ce8f2b9
    Signed-off-by: Max Chou <max.chou@realtek.com>
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2138373
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Shyh-In Hwang <josephsih@chromium.org>
    Commit-Queue: Shyh-In Hwang <josephsih@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -42,6 +42,7 @@
 
 static bool disable_scofix;
 static bool force_scofix;
+static bool enable_autosuspend = IS_ENABLED(CONFIG_BT_HCIBTUSB_AUTOSUSPEND);
 
 static bool reset = true;
 
@@ -3396,6 +3397,9 @@ static int btusb_probe(struct usb_interface *intf,
 	}
 #endif
 
+	if (enable_autosuspend)
+		usb_enable_autosuspend(data->udev);
+
 	err = hci_register_dev(hdev);
 	if (err < 0)
 		goto out_free_dev;
@@ -3612,6 +3616,9 @@ MODULE_PARM_DESC(disable_scofix, "Disable fixup of wrong SCO buffer size");
 
 module_param(force_scofix, bool, 0644);
 MODULE_PARM_DESC(force_scofix, "Force fixup of wrong SCO buffers size");
+
+module_param(enable_autosuspend, bool, 0644);
+MODULE_PARM_DESC(enable_autosuspend, "Enable USB autosuspend by default");
 
 module_param(reset, bool, 0644);
 MODULE_PARM_DESC(reset, "Send HCI reset command on initialization");
