796bdfb4de771fb4408fffaeb9ab69fe7580e047
Philipp Zabel <philipp.zabel@gmail.com>
2018-05-21 06:24:58 -0400

    UPSTREAM: media: uvcvideo: Fix driver reference counting
    kref_init initializes the reference count to 1, not 0. This additional
    reference is never released since the conversion to reference counters.
    As a result, uvc_delete is not called anymore when UVC cameras are
    disconnected.
    Fix this by adding an additional kref_put in uvc_disconnect and in the
    probe error path. This also allows to remove the temporary additional
    reference in uvc_unregister_video.
    
    Fixes: 9d15cd958c17 ("media: uvcvideo: Convert from using an atomic variable to a reference count")
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit f9ffcb0a21e1fa8e64d09ed613d884e054ae8191)
    
    BUG=chromium:1078293, b:172216354
    TEST=Camera tests
    
    Change-Id: I226fa8419d6c87a21ae779bae6c5f21c931a6ddd
    Disallow-Recycled-Builds: test-failures
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2578873
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1849,13 +1849,6 @@ static void uvc_unregister_video(struct uvc_device *dev)
 {
 	struct uvc_streaming *stream;
 
-	/* Unregistering all video devices might result in uvc_delete() being
-	 * called from inside the loop if there's no open file handle. To avoid
-	 * that, increment the refcount before iterating over the streams and
-	 * decrement it when done.
-	 */
-	kref_get(&dev->ref);
-
 	list_for_each_entry(stream, &dev->streams, list) {
 		if (!video_is_registered(&stream->vdev))
 			continue;
@@ -1864,8 +1857,6 @@ static void uvc_unregister_video(struct uvc_device *dev)
 
 		uvc_debugfs_cleanup_stream(stream);
 	}
-
-	kref_put(&dev->ref, uvc_delete);
 }
 
 static int uvc_register_video(struct uvc_device *dev,
@@ -2110,6 +2101,7 @@ static int uvc_probe(struct usb_interface *intf,
 
 error:
 	uvc_unregister_video(dev);
+	kref_put(&dev->ref, uvc_delete);
 	return -ENODEV;
 }
 
@@ -2127,6 +2119,7 @@ static void uvc_disconnect(struct usb_interface *intf)
 		return;
 
 	uvc_unregister_video(dev);
+	kref_put(&dev->ref, uvc_delete);
 }
 
 static int uvc_suspend(struct usb_interface *intf, pm_message_t message)
