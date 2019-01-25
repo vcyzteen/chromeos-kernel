2636d94b8084acaf85622a52fba09ebd44a76706
Vitaly Kuznetsov <vkuznets@redhat.com>
2019-01-25 12:19:34 +0100

    BACKPORT: x86/kvm/hyper-v: recommend using eVMCS only when it is enabled
    We shouldn't probably be suggesting using Enlightened VMCS when it's not
    enabled (not supported from guest's point of view). Hyper-V on KVM seems
    to be fine either way but let's be consistent.
    
    Fixes: 2bc39970e932 ("x86/kvm/hyper-v: Introduce KVM_GET_SUPPORTED_HV_CPUID")
    Reviewed-by: Liran Alon <liran.alon@oracle.com>
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit f1adceaf01f0446e69c15b32f24ce98e3c3623f1)
    
    [delco: Feature not present but 'fix' still backported to avoid
    warnings from bots searching for missing patches based on commit ID.]
    
    BUG=b:144746965
    TEST=Built
    Signed-off-by: Matt Delco <delco@chromium.org>
    
    Change-Id: I71c57bc07b1ceb006d19bf535df8d986b8568883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2036954
    Tested-by: Matt Delco <delco@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Commit-Queue: Matt Delco <delco@chromium.org>

--- arch/x86/kvm/hyperv.c
+++ arch/x86/kvm/hyperv.c
@@ -1183,7 +1183,8 @@ int kvm_vcpu_ioctl_get_hv_cpuid(struct kvm_vcpu *vcpu, struct kvm_cpuid2 *cpuid,
 			ent->eax |= HV_X64_RELAXED_TIMING_RECOMMENDED;
 			// ent->eax |= HV_X64_CLUSTER_IPI_RECOMMENDED;
 			// ent->eax |= HV_X64_EX_PROCESSOR_MASKS_RECOMMENDED;
-			// ent->eax |= HV_X64_ENLIGHTENED_VMCS_RECOMMENDED;
+			// if (kvm_x86_ops->nested_get_evmcs_version(vcpu))
+			//	ent->eax |= HV_X64_ENLIGHTENED_VMCS_RECOMMENDED;
 
 			/*
 			 * Default number of spinlock retry attempts, matches
