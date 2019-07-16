26c0fef8076eae3e06bf5a99a1b45a33e0223397
Fabien Lahoudere <fabien.lahoudere@collabora.com>
2019-07-16 11:11:06 +0200

    BACKPORT: iio: common: cros_ec_sensors: Expose cros_ec_sensors frequency range via iio sysfs
    Embedded controller return minimum and maximum frequencies, unfortunately
    we have no way to know the step for all available frequencies.
    Even if not complete, we can return a list of known values using the
    standard read_avail callback (IIO_CHAN_INFO_SAMP_FREQ) to provide them to
    userland.
    
    Now cros_ec_* sensors provides frequencies values in sysfs like this:
    "0 min max". 0 is always true to disable the sensor.
    
    Default frequencies are provided for earlier protocol.
    
    Conflicts:
    	drivers/iio/common/cros_ec_sensors/cros_ec_sensors_core.c
                    Comment for _init() still here.
    	drivers/iio/light/cros_ec_light_prox.c
                    light driver in common/cros_ec_sensors.
    	include/linux/iio/common/cros_ec_sensors_core.h
                    scale fix already in place.
    
    BUG=chromium:946178
    TEST=compile
    
    Signed-off-by: Nick Vaccaro <nvaccaro@chromium.org>
    Signed-off-by: Fabien Lahoudere <fabien.lahoudere@collabora.com>
    [rebased on top of iio/testing and solved conflicts]
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    (cherry picked from commit ae7b02ad2f32d39d1434655f346c04a16c1aa703)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: Ie83baf16c4dfcf6eb7a8814fd511c3171b74bc23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2234829

--- include/linux/iio/common/cros_ec_sensors_core.h
+++ include/linux/iio/common/cros_ec_sensors_core.h
@@ -100,6 +100,9 @@ struct cros_ec_sensors_core_state {
 	/* Current sampling period */
 	int curr_sampl_freq;
 
+	/* Table of known available frequencies : 0, Min and Max in mHz */
+	int frequencies[3];
+
 	/* Saved ranage if modified */
 	bool range_updated;
 	int curr_range;
@@ -148,12 +151,30 @@ int cros_ec_motion_send_host_cmd(struct cros_ec_sensors_core_state *st,
  * cmd_lock lock must be held.
  */
 int cros_ec_sensors_core_read(struct cros_ec_sensors_core_state *st,
-			  struct iio_chan_spec const *chan,
-			  int *val, int *val2, long mask);
+			      struct iio_chan_spec const *chan,
+			      int *val, int *val2, long mask);
 
 int cros_ec_sensors_core_write(struct cros_ec_sensors_core_state *st,
 			       struct iio_chan_spec const *chan,
 			       int val, int val2, long mask);
+
+/**
+ * cros_ec_sensors_core_read_avail() - get available values
+ * @indio_dev:		pointer to state information for device
+ * @chan:	channel specification structure table
+ * @vals:	list of available values
+ * @type:	type of data returned
+ * @length:	number of data returned in the array
+ * @mask:	specifies which values to be requested
+ *
+ * Return:	an error code, IIO_AVAIL_RANGE or IIO_AVAIL_LIST
+ */
+int cros_ec_sensors_core_read_avail(struct iio_dev *indio_dev,
+				    struct iio_chan_spec const *chan,
+				    const int **vals,
+				    int *type,
+				    int *length,
+				    long mask);
 
 extern const struct dev_pm_ops cros_ec_sensors_pm_ops;
 
