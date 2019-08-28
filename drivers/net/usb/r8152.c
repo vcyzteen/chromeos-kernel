7c3a7cd21faa83b44b4734202a05194723767195
Hayes Wang <hayeswang@realtek.com>
2019-08-28 09:51:42 +0800

    BACKPORT: r8152: remove calling netif_napi_del
    Remove unnecessary use of netif_napi_del. This also avoids to call
    napi_disable() after netif_napi_del().
    
    Signed-off-by: Hayes Wang <hayeswang@realtek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 973dc6cfc0e2c43ff29ca5645ceaf1ae694ea110)
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    
    BUG=chromium:1026885
    TEST=tast --verbose run DUT power.Reboot passed 200 times
         on an eve device.
    
    Change-Id: I3d73c79ac4b1f155251bbb02b3067ba4bc9b7276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1937873
    Reviewed-by: Grant Grundler <grundler@chromium.org>
    Auto-Submit: Prashant Malani <pmalani@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -5866,7 +5866,6 @@ static int rtl8152_probe(struct usb_interface *intf,
 	return 0;
 
 out1:
-	netif_napi_del(&tp->napi);
 	tasklet_kill(&tp->tx_tl);
 	usb_set_intfdata(intf, NULL);
 out:
@@ -5882,7 +5881,6 @@ static void rtl8152_disconnect(struct usb_interface *intf)
 	if (tp) {
 		rtl_set_unplug(tp);
 
-		netif_napi_del(&tp->napi);
 		unregister_netdev(tp->netdev);
 		tasklet_kill(&tp->tx_tl);
 		cancel_delayed_work_sync(&tp->hw_phy_work);
