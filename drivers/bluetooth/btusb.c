ad0efd4854bc4df9f8cea60f69e083d2c9945910
Amit K Bag <amit.k.bag@intel.com>
2019-10-17 13:52:29 +0530

    BACKPORT: Bluetooth: btusb: Trigger Intel FW download error recovery
    Sometimes during FW data download stage, in case of an error is
    encountered the controller device could not be recovered. To recover
    from such failures send Intel hard Reset to re-trigger FW download in
    following error scenarios:
    
    1. Intel Read version command error
    2. Firmware download timeout
    3. Failure in Intel Soft Reset for switching to operational FW
    4. Boot timeout for switching to operaional FW
    
    Signed-off-by: Raghuram Hegde <raghuram.hegde@intel.com>
    Signed-off-by: Chethan T N <chethan.tumkur.narayan@intel.com>
    Signed-off-by: Amit K Bag <amit.k.bag@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    
    (cherry picked from commit b9a2562f4918c557f664fbba215122aca3cbb2fe)
    
    Conflicts:
       drivers/bluetooth/btusb.c
    
    BUG=b:170512557
    TEST=None
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Change-Id: I59ba1becd588c22bd4a082ffe54abcc2c27ef48a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2469803
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Reviewed-by: Yu Liu <yudiliu@google.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -2221,8 +2221,11 @@ static int btusb_setup_intel_new(struct hci_dev *hdev)
 	 * loaded.
 	 */
 	err = btintel_read_version(hdev, &ver);
-	if (err)
+	if (err) {
+		bt_dev_err(hdev, "Intel Read version failed (%d)", err);
+		btintel_reset_to_bootloader(hdev);
 		return err;
+	}
 
 	/* The hardware platform number has a fixed value of 0x37 and
 	 * for now only accept this single value.
@@ -2388,9 +2391,13 @@ static int btusb_setup_intel_new(struct hci_dev *hdev)
 
 	/* Start firmware downloading and get boot parameter */
 	err = btintel_download_firmware(hdev, fw, &boot_param);
-	if (err < 0)
+	if (err < 0) {
+		/* When FW download fails, send Intel Reset to retry
+		 * FW download.
+		 */
+		btintel_reset_to_bootloader(hdev);
 		goto done;
-
+	}
 	set_bit(BTUSB_FIRMWARE_LOADED, &data->flags);
 
 	BT_INFO("%s: Waiting for firmware download to complete", hdev->name);
@@ -2417,6 +2424,7 @@ static int btusb_setup_intel_new(struct hci_dev *hdev)
 	if (err) {
 		BT_ERR("%s: Firmware loading timeout", hdev->name);
 		err = -ETIMEDOUT;
+		btintel_reset_to_bootloader(hdev);
 		goto done;
 	}
 
@@ -2452,8 +2460,11 @@ done:
 	set_bit(BTUSB_BOOTING, &data->flags);
 
 	err = btintel_send_intel_reset(hdev, boot_param);
-	if (err)
+	if (err) {
+		bt_dev_err(hdev, "Intel Soft Reset failed (%d)", err);
+		btintel_reset_to_bootloader(hdev);
 		return err;
+	}
 
 	/* The bootloader will not indicate when the device is ready. This
 	 * is done by the operational firmware sending bootup notification.
@@ -2474,7 +2485,8 @@ done:
 	}
 
 	if (err) {
-		BT_ERR("%s: Device boot timeout", hdev->name);
+		bt_dev_err(hdev, "Device boot timeout");
+		btintel_reset_to_bootloader(hdev);
 		return -ETIMEDOUT;
 	}
 
