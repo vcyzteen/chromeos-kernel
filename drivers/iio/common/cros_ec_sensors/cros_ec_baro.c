22697e5652cbf02ae66eb205e2c2578b4eecab28
Gwendal Grignou <gwendal@chromium.org>
2019-11-06 09:55:33 -0800

    BACKPORT: iio: cros_ec_baro: set info_mask_shared_by_all_available field
    Field was already set for light/proximity and
    accelerometer/gyroscope/magnetometer sensors.
    
    Fixes: ae7b02ad2f32 ("iio: common: cros_ec_sensors: Expose cros_ec_sensors frequency range via iio sysfs")
    
    BUG=chromium:946178
    TEST=tast run <ip> hardware.SensorRing with sensor ring enabled.
         iio-info with sensor ring disabled.
    
    Conflicts:
    	drivers/iio/pressure/cros_ec_baro.c:
                     driver in common/cros_ec_sensors.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    (cherry picked from commit e9a4cbcaaa391ef44d623d548ee715e77265030c)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: I370317cafea105b1a25916525e8770f6d6da44e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2234845

--- drivers/iio/common/cros_ec_sensors/cros_ec_baro.c
+++ drivers/iio/common/cros_ec_sensors/cros_ec_baro.c
@@ -124,6 +124,7 @@ static const struct iio_info cros_ec_baro_info = {
 	.read_raw = &cros_ec_baro_read,
 	.write_raw = &cros_ec_baro_write,
 	.driver_module = THIS_MODULE,
+	.read_avail = &cros_ec_sensors_core_read_avail,
 };
 
 static int cros_ec_baro_probe(struct platform_device *pdev)
@@ -163,6 +164,8 @@ static int cros_ec_baro_probe(struct platform_device *pdev)
 		BIT(IIO_CHAN_INFO_SCALE) |
 		BIT(IIO_CHAN_INFO_SAMP_FREQ) |
 		BIT(IIO_CHAN_INFO_FREQUENCY);
+	channel->info_mask_shared_by_all_available =
+		BIT(IIO_CHAN_INFO_SAMP_FREQ);
 	channel->scan_type.realbits = CROS_EC_SENSOR_BITS;
 	channel->scan_type.storagebits = CROS_EC_SENSOR_BITS;
 	channel->scan_type.shift = 0;
