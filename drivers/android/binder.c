9c8d75ecf42c8fe68d3062c2b7da237ebfc3d42b
Sergey Senozhatsky <senozhatsky@chromium.org>
2020-02-18 13:25:38 +0900

    Revert "CHROMIUM: android: binder: get rid of BINDER_LOOPER_STATE_WAITING"
    This reverts commit 0328ce2cf3d502c86dc4fd34996a4ecb7c8e91a1.
    
    This is are preparation patch before we bring binder to what is in the
    Android 4.4 common kernel tree.
    
    BUG=b:142440806
    Change-Id: I56baca96f6038f608520ff6118d2b6c9f2f4cddf
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2703,6 +2703,7 @@ retry:
 	}
 
 
+	thread->looper |= BINDER_LOOPER_STATE_WAITING;
 	if (wait_for_proc_work)
 		proc->ready_threads++;
 
@@ -2737,6 +2738,7 @@ retry:
 
 	if (wait_for_proc_work)
 		proc->ready_threads--;
+	thread->looper &= ~BINDER_LOOPER_STATE_WAITING;
 
 	if (ret)
 		return ret;
@@ -3573,14 +3575,16 @@ static void binder_deferred_flush(struct binder_proc *proc)
 		struct binder_thread *thread = rb_entry(n, struct binder_thread, rb_node);
 
 		thread->looper |= BINDER_LOOPER_STATE_NEED_RETURN;
-		wake_up(&thread->wait);
-		wake_count++;
+		if (thread->looper & BINDER_LOOPER_STATE_WAITING) {
+			wake_up_interruptible(&thread->wait);
+			wake_count++;
+		}
 	}
 	wake_up_interruptible_all(&proc->wait);
 
 	binder_debug(BINDER_DEBUG_OPEN_CLOSE,
-		     "binder_flush: %d attempted to wake %d threads\n",
-		     proc->pid, wake_count);
+		     "binder_flush: %d woke %d threads\n", proc->pid,
+		     wake_count);
 }
 
 static int binder_release(struct inode *nodp, struct file *filp)
