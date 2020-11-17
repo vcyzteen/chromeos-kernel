298c597bfeeb11165b8136c66a5d6a03af2b4513
Yu Zhao <yuzhao@google.com>
2020-11-17 12:40:44 -0700

    CHROMIUM: mm: fix tlb flush race between page table and rmap walkers
    We saw memory corruptions on Chromebooks equipped with arm/arm64. It
    turned out there is a race between per-process reclaim that walks page
    table and calls ptep_test_and_clear_young() and other rmap walkers
    (e.g., writeback and kswapd/direct reclaims) that calls
    ptep_clear_flush().
    
    On arm/arm64, clearing the accessed bit on a PTE without flushing TLB
    is a bug because of the optimization built into pte_accessible(). By
    default, if the accessed bit isn't set on a PTE, then it can't have
    been used and therefore flushing TLB after downgrading permissions is
    unnecessary. If anybody wants to clear the accessed bit, they have to
    raise a flag so others know that the accessed bit is unreliable and
    have to take the alternative path.
    
    However, per-process reclaim breaks this rule and becomes vulnerable
    to the following race:
    
    per-process reclaim		writeback etc.		userspace
    
    walk page tables		walk rmap
      ptep_test_and_clear_young()
    				  ptep_clear_flush()
    				    pte_accessible()
    				      skip flush
    				writeback or swap page
    							modify page
      flush_tlb_mm()
    				free page
    
    To fix the race, we raise the flag by tlb_gather_mmu() before walking
    page tables. Other rmap walkers will see it by mm_tlb_flush_pending().
    We call tlb_finish_mmu() at the end to do a full-range flush, which
    is a necessary evil. The harm to other architectures is limited since
    the process is usually cold.
    
    BUG=b:166311876
    TEST=built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: Ifb7ed79eef3691e4b4637f0fad1e3eebe904d5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2553084
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Auto-Submit: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1762,6 +1762,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	struct vm_area_struct *vma;
 	enum reclaim_type type;
 	char *type_buf;
+	struct mmu_gather tlb;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -1792,6 +1793,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 		};
 
 		down_read(&mm->mmap_sem);
+		tlb_gather_mmu(&tlb, mm, 0, -1);
 		for (vma = mm->mmap; vma; vma = vma->vm_next) {
 			if (is_vm_hugetlb_page(vma))
 				continue;
@@ -1812,7 +1814,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 			walk_page_range(vma->vm_start, vma->vm_end,
 					&reclaim_walk);
 		}
-		flush_tlb_mm(mm);
+		tlb_finish_mmu(&tlb, 0, -1);
 		up_read(&mm->mmap_sem);
 		mmput(mm);
 	}
