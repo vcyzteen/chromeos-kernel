2e9ca0806f1595aa652e4826556c3d1c2142441b
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2020-12-16 03:04:40 +0000

    UPSTREAM: drm/doc: Document that modifiers are always required for fb
    Even for legacy userspace, since otherwise GETFB2 is broken and if you
    switch between modifier-less and modifier-aware compositors, smooth
    transitions break.
    
    Also it's just best practice to make sure modifiers are invariant for
    a given drm_fb, and that a modifier-aware kms drivers only has one
    place to store them, ignoring any old implicit bo flags or whatever
    else might float around.
    
    Motivated by some irc discussion with Bas about amdgpu modifier
    support.
    
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Acked-by: Simon Ser <contact@emersion.fr>
    Acked-by: Daniel Stone <daniels@collabora.com>
    Acked-by: Pekka Paalanen <pekka.paalanen@collabora.com>
    Fixes: 455e00f1412f ("drm: Add getfb2 ioctl")
    Cc: Daniel Stone <daniels@collabora.com>
    Cc: Juston Li <juston.li@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Cc: Marek Olšák <maraeo@gmail.com>
    Cc: "Wentland, Harry" <harry.wentland@amd.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200917164721.2038541-1-daniel.vetter@ffwll.ch
    (cherry picked from commit 9e9f605fb5af8ae350d59de3c2a54422cb2d9301)
    
    BUG=b:138963067,chromium:934848
    TEST=i-g-t kms_getfb tests
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: I0248cd278d0563425a615d436b452449c387db53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2594673
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>

--- include/drm/drm_mode_config.h
+++ include/drm/drm_mode_config.h
@@ -50,6 +50,12 @@ struct drm_mode_config_funcs {
 	 * requested metadata, but most of that is left to the driver. See
 	 * struct &drm_mode_fb_cmd2 for details.
 	 *
+	 * IMPORTANT: These implied modifiers for legacy userspace must be
+	 * stored in struct &drm_framebuffer, including all relevant metadata
+	 * like &drm_framebuffer.pitches and &drm_framebuffer.offsets if the
+	 * modifier enables additional planes beyond the fourcc pixel format
+	 * code. This is required by the GETFB2 ioctl.
+	 *
 	 * If the parameters are deemed valid and the backing storage objects in
 	 * the underlying memory manager all exist, then the driver allocates
 	 * a new &drm_framebuffer structure, subclassed to contain
@@ -670,6 +676,13 @@ struct drm_mode_config {
 	 * @allow_fb_modifiers:
 	 *
 	 * Whether the driver supports fb modifiers in the ADDFB2.1 ioctl call.
+	 *
+	 * IMPORTANT:
+	 *
+	 * If this is set the driver must fill out the full implicit modifier
+	 * information in their &drm_mode_config_funcs.fb_create hook for legacy
+	 * userspace which does not set modifiers. Otherwise the GETFB2 ioctl is
+	 * broken for modifier aware userspace.
 	 */
 	bool allow_fb_modifiers;
 
