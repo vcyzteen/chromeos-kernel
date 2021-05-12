a7f46fe03f7de9f42d693e8f3394a8356560ecc0
Wen Gong <wgong@codeaurora.org>
2021-05-12 04:47:50 -0400

    BACKPORT: UPSTREAM: ath10k: add struct for high latency PN replay protection
    Add the struct for PN replay protection and fragment packet
    handler.
    
    Also fix the bitmask of HTT_RX_DESC_HL_INFO_MCAST_BCAST to match what's currently
    used by SDIO firmware. The defines are not used yet so it's safe to modify
    them. Remove the conflicting HTT_RX_DESC_HL_INFO_FRAGMENT as
    it's not either used in ath10k.
    
    Tested on QCA6174 SDIO with firmware WLAN.RMH.4.4.1-00007-QCARMSWP-1.
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    
    (cherry picked from commit e1bddde9737ac4687ca6e2fe6c95f67a9bec353b)
    
    Conflicts:
       drivers/net/wireless/ar10k/ath/ath10k/htt.h
       [Ported from ath10k to ar10k]
    
    BUG=b:187853103, b:187853456
    TEST=Verified wifi_matfunc/perf
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    Change-Id: Icacc0de033f40ab36b9dc72accae60a5e5bada15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2891957
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/ar10k/ath/ath10k/htt.h
+++ drivers/net/wireless/ar10k/ath/ath10k/htt.h
@@ -642,6 +642,20 @@ struct htt_rx_indication {
 	struct htt_rx_indication_mpdu_range mpdu_ranges[0];
 } __packed;
 
+struct htt_hl_rx_desc {
+	__le32 info;
+	__le32 pn_31_0;
+	union {
+		struct {
+			__le16 pn_47_32;
+			__le16 pn_63_48;
+		} pn16;
+		__le32 pn_63_32;
+	} u0;
+	__le32 pn_95_64;
+	__le32 pn_127_96;
+} __packed;
+
 static inline struct htt_rx_indication_mpdu_range *
 		htt_rx_ind_get_mpdu_ranges(struct htt_rx_indication *rx_ind)
 {
@@ -687,6 +701,21 @@ struct htt_rx_peer_unmap {
 	__le16 peer_id;
 } __packed;
 
+enum htt_txrx_sec_cast_type {
+	HTT_TXRX_SEC_MCAST = 0,
+	HTT_TXRX_SEC_UCAST
+};
+
+enum htt_rx_pn_check_type {
+	HTT_RX_NON_PN_CHECK = 0,
+	HTT_RX_PN_CHECK
+};
+
+enum htt_rx_tkip_demic_type {
+	HTT_RX_NON_TKIP_MIC = 0,
+	HTT_RX_TKIP_MIC
+};
+
 enum htt_security_types {
 	HTT_SECURITY_NONE,
 	HTT_SECURITY_WEP128,
@@ -699,6 +728,9 @@ enum htt_security_types {
 
 	HTT_NUM_SECURITY_TYPES /* keep this last! */
 };
+
+#define ATH10K_HTT_TXRX_PEER_SECURITY_MAX 2
+#define ATH10K_TXRX_NUM_EXT_TIDS 19
 
 enum htt_security_flags {
 #define HTT_SECURITY_TYPE_MASK 0x7F
@@ -809,6 +841,11 @@ struct htt_rx_fragment_indication {
 
 	u8 fw_msdu_rx_desc[0];
 } __packed;
+
+#define ATH10K_IEEE80211_EXTIV               BIT(5)
+#define ATH10K_IEEE80211_TKIP_MICLEN         8   /* trailing MIC */
+
+#define HTT_RX_FRAG_IND_INFO0_HEADER_LEN     16
 
 #define HTT_RX_FRAG_IND_INFO0_EXT_TID_MASK     0x1F
 #define HTT_RX_FRAG_IND_INFO0_EXT_TID_LSB      0
